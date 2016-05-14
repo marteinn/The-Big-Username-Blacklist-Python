960a9a8aa8844dbcfc7699ba0e5178dd2075f011
Martin Sandström <martin@marteinn.se>
2016-05-14 22:15:40 +0200

    Bumped version

--- the_big_username_blacklist/__init__.py
+++ the_big_username_blacklist/__init__.py
@@ -3,14 +3,15 @@
 """
 the-big-username-blacklist
 --------------------------
+A opinionated username blacklist
 """
 
 __title__ = "the_big_username_blacklist"
-__version__ = "1.3.2"
-__build__ = 132
+__version__ = "1.3.3"
+__build__ = 133
 __author__ = "Martin Sandström"
 __license__ = "MIT"
-__copyright__ = "Copyright 2015 Martin Sandström"
+__copyright__ = "Copyright 2015-2016 Martin Sandström"
 
 
 from .validator import validate, get_blacklist  # NOQA
